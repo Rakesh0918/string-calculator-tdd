49b79aa19a1500c9d821086d05c6daa486ba6239
Rakesh0918 <rs7937753@gmail.com>
2025-06-26 20:25:09 +0530

    Initial commit with Xcode project and empty calculator

---
+++ StringCalculatorTDD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StringCalculatorTDD
+//
+//  Created by Rakesh on 26/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
