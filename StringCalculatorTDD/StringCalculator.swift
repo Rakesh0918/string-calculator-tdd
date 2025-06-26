978bffb484598fe5b88955cbfd4cba4c4d5b8f18
Rakesh0918 <rs7937753@gmail.com>
2025-06-26 21:52:43 +0530

    Add support for custom delimiter using //;\n syntax

--- StringCalculatorTDD/StringCalculator.swift
+++ StringCalculatorTDD/StringCalculator.swift
@@ -11,9 +11,25 @@ final class StringCalculator {
     
     func add(_ numbers: String) throws -> Int {
         guard !numbers.isEmpty else { return 0 }
-        let numbers = numbers.replacingOccurrences(of: "\n", with: ",")
-        let tokens = numbers.components(separatedBy: ",").compactMap { Int($0) }
-        return tokens.reduce(0, +)
+        
+        var delimiter = ","
+        var input = numbers
+        
+        if numbers.hasPrefix("//") {
+            let parts = numbers.components(separatedBy: "\n")
+            if let delimiterPart = parts.first?.dropFirst(2) {
+                delimiter = String(delimiterPart)
+                input = parts.dropFirst().joined(separator: "\n")
+            }
+        }
+        
+        let components = input
+            .replacingOccurrences(of: "\n", with: delimiter)
+            .components(separatedBy: delimiter)
+        
+        let ints = components.compactMap { Int($0) }
+        
+        return ints.reduce(0, +)
     }
     
 }
